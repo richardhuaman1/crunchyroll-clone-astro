d97d6de3ef259cef578b0a058d291fd724fb9417
Richard Huaman <richardhuaman.dev@gmail.com>
2024-07-26 17:54:24 -0500

    feat: add show card  component

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -4,3 +4,11 @@ export const LAYOUT_SIZES = {
 };
 
 export const LAYOUT_TOTAL_HEIGHT = LAYOUT_SIZES.navbar + LAYOUT_SIZES.footer;
+
+export const BREAKPOINTS = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+} as const;
